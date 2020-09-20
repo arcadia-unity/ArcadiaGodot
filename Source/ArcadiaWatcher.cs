6034146f521c3b60918a4977a7eaa6b2de58ef9e
selfsame <joseph.parker.1@gmail.com>
2020-09-20 14:43:19 -0400

    get ArcadiaWatcher working in linux

--- Source/ArcadiaWatcher.cs
+++ Source/ArcadiaWatcher.cs
@@ -1,11 +1,19 @@
+using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Threading;
 using System.Security.Permissions;
+using System.Text.RegularExpressions;
 using clojure.lang;
 using Godot;
 
 public class ArcadiaWatcher
 {
-    public FileSystemWatcher watcher;
+    private FileSystemWatcher watcher;
+    private Regex clj_file_regex = new Regex(@"\.clj[c]?$",
+          RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    
+    private Dictionary<string, bool> changed_files = new Dictionary<string, bool>();
 
     [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
     public ArcadiaWatcher()
@@ -13,27 +21,51 @@ public class ArcadiaWatcher
         watcher = new FileSystemWatcher();
         watcher.Path = ProjectSettings.GlobalizePath("res://");
         watcher.IncludeSubdirectories = true;
-        watcher.NotifyFilter = NotifyFilters.LastAccess
-                                | NotifyFilters.LastWrite
-                                | NotifyFilters.FileName
-                                | NotifyFilters.DirectoryName;
-        watcher.Filter = "*.clj?";
+        watcher.NotifyFilter =  NotifyFilters.FileName | NotifyFilters.Size;
+
+        //watcher.Filter = "*.clj?";
         watcher.Changed += OnChanged;
         watcher.Created += OnChanged;
         watcher.Renamed += OnChanged;
         watcher.EnableRaisingEvents = true;
+
+        System.Threading.Timer timer = new System.Threading.Timer(this.DrainChanges, new AutoResetEvent(false), 100, 100);
+
         GD.Print("watching .clj files in ", watcher.Path);
     }
 
-    private static void OnChanged(object source, FileSystemEventArgs e) {
-        try
+    private void OnChanged(object source, FileSystemEventArgs e) {
+        
+        if (clj_file_regex.Matches(e.Name).Count > 0)
         {
-            RT.load("clojure.core");
-            Arcadia.Util.Invoke(RT.var("clojure.core", "load-file"), e.Name);
+            try
+            {
+                lock (changed_files){
+                    changed_files[e.Name] = true;
+                }
+            }
+            catch (System.Exception err)
+            {
+                GD.PrintErr(err);
+            }
+            finally{
+                
+            }
         }
-        catch (System.Exception err)
+    }
+
+    private void DrainChanges(System.Object stateInfo){
+        lock (changed_files)
         {
-            GD.PrintErr(err);
+            foreach (var path in changed_files.Keys){
+                
+                if (path != null){
+                    GD.Print("reloading ", path);
+                    Arcadia.Util.Invoke(RT.var("clojure.core", "load-file"), path);
+                }
+                
+            }
+            changed_files.Clear();
         }
     }
 }
