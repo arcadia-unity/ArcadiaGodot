50e64067f571668b00999765adc6ccf0780c807e
Arun Dilipan <arundilipan@gmail.com>
2020-12-01 14:38:36 -0500

    Added `move-and-slide`

--- Source/arcadia/2D.clj
+++ Source/arcadia/2D.clj
@@ -1,9 +1,30 @@
 (ns arcadia.2D
   (:import 
-    [Godot Node GD Node2D Vector2]))
+    [Godot Node GD Node2D Vector2 KinematicBody2D]))
 
 (defn ^Vector2 position [^Node2D node]
   (.Position node))
 
 (defn ^Vector2 position! [^Node2D node ^Vector2 v]
   (.SetPosition node v))
+
+(defn ^Vector2 move-and-slide
+  [^KinematicBody2D o
+   ^Vector2 v
+   & {:keys [floor-normal
+             stop-on-slope?
+             max-slides
+             floor-max-angle
+             infinite-inertia?]
+      :or {floor-normal (v2)
+           stop-on-slope? false
+           max-slides 4
+           floor-max-angle 0.785398
+           infinite-inertia? true}}]
+  (.MoveAndSlide o
+                 v
+                 floor-normal
+                 stop-on-slope?
+                 max-slides
+                 floor-max-angle
+                 infinite-inertia?))
--- Source/arcadia/3D.clj
+++ Source/arcadia/3D.clj
@@ -1,9 +1,30 @@
 (ns arcadia.3D
   (:import 
-    [Godot Node GD SceneTree Spatial Vector3]))
+    [Godot Node GD SceneTree Spatial Vector3 KinematicBody]))
 
 (defn ^Vector3 translation [^Spatial o]
   (.Translation o))
 
 (defn ^Vector3 translation! [^Spatial o ^Vector3 v]
   (.SetTranslation o v))
+
+(defn ^Vector3 move-and-slide
+  [^KinematicBody o
+   ^Vector3 v
+   & {:keys [floor-normal
+             stop-on-slope?
+             max-slides
+             floor-max-angle
+             infinite-inertia?]
+      :or {floor-normal (v2)
+           stop-on-slope? false
+           max-slides 4
+           floor-max-angle 0.785398
+           infinite-inertia? true}}]
+  (.MoveAndSlide o
+                 v
+                 floor-normal
+                 stop-on-slope?
+                 max-slides
+                 floor-max-angle
+                 infinite-inertia?))
