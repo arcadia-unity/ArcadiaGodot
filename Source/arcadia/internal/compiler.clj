29d4e2d9cdef9858349f753d75f07faae84a1be5
Kevin W. van Rooijen <kevin.van.rooijen@protonmail.com>
2021-06-17 20:30:28 +0200

    Add compiler scene to compile Clojure files

--- Source/arcadia/internal/compiler.clj
+++ Source/arcadia/internal/compiler.clj
@@ -1,11 +1,12 @@
 (ns arcadia.internal.compiler
   (:use
-    arcadia.core)
+   arcadia.core)
   (:require
-    clojure.string
-    [arcadia.internal.config :as config])
+   [arcadia.internal.cmd :as cmd]
+   [clojure.string :as string]
+   [arcadia.internal.config :as config])
   (:import
-    [Arcadia.Boot]))
+   [Arcadia.Boot]))
 
 (defn- aot-namespaces [path nss]
    ;; We want to ensure that namespaces are neither double-aot'd, nor
@@ -30,12 +31,64 @@
        (alter @#'clojure.core/*loaded-libs* into @loaded-libs'))
      nil))
 
-(defn aot [path ns-syms]
+(defn aot
   "aot ns-syms to the given path, with all dependencies"
+  [path ns-syms]
   (Arcadia.Boot/SetClojureLoadPath)
-  (aot-namespaces path (concat ns-syms [
-    'clojure.core
-    'clojure.core.server
-    'arcadia.internal.namespace
-    'arcadia.repl]))
+  (aot-namespaces path (concat ns-syms ['clojure.core
+                                        'clojure.core.server
+                                        'arcadia.internal.namespace
+                                        'arcadia.repl]))
   (Arcadia.Boot/SetClojureLoadPathWithDLLs))
+(defn- copy-infrastructure! [dlls-dir]
+  (doseq [file (System.IO.Directory/GetFiles "ArcadiaGodot/Infrastructure")
+          :when (string/ends-with? file "dll")]
+    (let [file-name (last (string/split file #"/"))]
+      (System.IO.File/Copy file (System.IO.Path/Combine dlls-dir file-name) true))))
+
+(defn- compile-project! [target namespaces]
+  (let [dlls-dir (System.IO.Path/Combine target "dlls")]
+    (System.IO.Directory/CreateDirectory dlls-dir)
+    (copy-infrastructure! dlls-dir)
+    (aot dlls-dir namespaces)))
+
+(defn- cleanup-cmdline-args [cmdline-args]
+  (let [current (:current cmdline-args)]
+    (cond-> cmdline-args
+      current (update :flags conj (keyword current))
+      true (dissoc :current))))
+
+(defn- cmdline-args []
+  (-> (vec (Godot.OS/GetCmdlineArgs))
+      (cmd/parse-opts)
+      (cleanup-cmdline-args)))
+
+(defn- return-argument-error []
+  (println "ERROR: --src <PATH> and --target <PATH> required")
+  (.Quit (arcadia.core/tree) 1))
+
+(defn- clojure-namespaces [src]
+  (for [file (System.IO.Directory/GetFiles src "*.clj", SearchOption/AllDirectories)
+        :when (not (string/includes? file "ArcadiaGodot"))]
+    (-> file
+        (subs (inc (count src)))
+        (string/replace #"\/" ".")
+        (string/replace #"\.clj$" "")
+        (string/replace #"_" "-")
+        (symbol))))
+
+(defn- compile-and-exit [{:keys [target src]}]
+  (compile-project! target (clojure-namespaces src))
+  (.Quit (arcadia.core/tree) 0))
+
+(defn ready [_ _]
+  (try
+    (let [{:keys [pairs]} (cmdline-args)]
+      (if (and (:src pairs)
+               (:target pairs))
+        (compile-and-exit pairs)
+        (return-argument-error)))
+    (catch Exception e
+      (println "ERROR: Compilation failure!")
+      (println e)
+      (.Quit (arcadia.core/tree) 1))))
