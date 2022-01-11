d24cd76ae193e62fc37ca76af33a980a480fb5bb
Aidar Lukmanov <aydarluk@gmail.com>
2022-01-11 22:44:14 +0500

    fix indentation

--- Source/Boot.cs
+++ Source/Boot.cs
@@ -38,7 +38,7 @@ namespace Arcadia {
             Util.LoadAssembliesFromDirectory("res://addons/ArcadiaGodot/Infrastructure");
 			
             RT.Initialize(doRuntimePostBoostrap: false);
-			RT.TryLoadInitType("clojure/core");
+            RT.TryLoadInitType("clojure/core");
             RT.TryLoadInitType("magic/api");
             
             if (OS.HasFeature("standalone")) { 
@@ -47,10 +47,10 @@ namespace Arcadia {
             } else {
                 RT.var("clojure.core", "*load-fn*").bindRoot(RT.var("clojure.core", "-load"));
             }
-			RT.var("clojure.core", "*eval-form-fn*").bindRoot(RT.var("magic.api", "eval"));
-			RT.var("clojure.core", "*load-file-fn*").bindRoot(RT.var("magic.api", "runtime-load-file"));
-			RT.var("clojure.core", "*compile-file-fn*").bindRoot(RT.var("magic.api", "runtime-compile-file"));
-			RT.var("clojure.core", "*macroexpand-1-fn*").bindRoot(RT.var("magic.api", "runtime-macroexpand-1"));
+            RT.var("clojure.core", "*eval-form-fn*").bindRoot(RT.var("magic.api", "eval"));
+            RT.var("clojure.core", "*load-file-fn*").bindRoot(RT.var("magic.api", "runtime-load-file"));
+            RT.var("clojure.core", "*compile-file-fn*").bindRoot(RT.var("magic.api", "runtime-compile-file"));
+            RT.var("clojure.core", "*macroexpand-1-fn*").bindRoot(RT.var("magic.api", "runtime-macroexpand-1"));
 
             RT.load("nostrand/core");
             SetClojureLoadPath();
