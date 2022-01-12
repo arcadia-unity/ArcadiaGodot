c3380a49eb810461ba34bc70c63832bafd9b5006
Aidar Lukmanov <aydarluk@gmail.com>
2022-01-12 19:58:03 +0500

    fix recompile infrastructure arcadia internal filesystem with each build

--- Source/arcadia/internal/compiler.clj
+++ Source/arcadia/internal/compiler.clj
@@ -10,6 +10,16 @@
 (def arcadia-nss 
   ['arcadia.internal.namespace 'arcadia.internal.config 'arcadia.repl 'arcadia.internal.nrepl-support])
 
+(defn recompile-filesystem []
+  (let [compile-path Paths/Infrastructure
+        file-res-path (. Path Combine "res://" compile-path (str "arcadia.internal.filesystem.clj.dll"))]
+    (arc/log "Recompile arcadia.internal.filesystem in directory: " Paths/Infrastructure)
+    (binding [*compile-path* compile-path
+              clojure.core/*loaded-libs* (ref (sorted-set))]
+      (Util/RemoveFile file-res-path)
+      (arc/log "compiling arcadia.internal.filesystem")
+      (compile 'arcadia.internal.filesystem))))
+
 (defn prepare-export []
   (let [export-nss (config/get-config-key :export-namespaces)
         nss (concat arcadia-nss export-nss)
@@ -21,14 +31,6 @@
                (let [file-res-path (. Path Combine "res://" compile-path (str ns ".clj.dll") )]
                  (Util/RemoveFile file-res-path)
                  (arc/log "compiling " ns)
-                 (compile ns)))))))
-
-(defn recompile-filesystem []
-  (let [compile-path Paths/Infrastructure
-        file-res-path (. Path Combine "res://" compile-path (str "arcadia.internal.filesystem.clj.dll"))]
-    (arc/log "Recompile arcadia.internal.filesystem in directory: " Paths/Infrastructure)
-    (binding [*compile-path* compile-path
-              clojure.core/*loaded-libs* (ref (sorted-set))]
-      (Util/RemoveFile file-res-path)
-      (arc/log "compiling arcadia.internal.filesystem")
-      (compile 'arcadia.internal.filesystem))))
+                 (compile ns)))))
+    (recompile-filesystem)
+    ))
